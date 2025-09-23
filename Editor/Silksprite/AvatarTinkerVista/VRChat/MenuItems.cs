cc6fc3eaa1a73e352e56f007e56c62698dd4f6e8
kaikoga <info@mail.kaikoga.net>
2025-09-24 07:31:10 +0900

    fix naming

--- Editor/Silksprite/AvatarTinkerVista/VRChat/MenuItems.cs
+++ Editor/Silksprite/AvatarTinkerVista/VRChat/MenuItems.cs
@@ -4,8 +4,8 @@ namespace Silksprite.AvatarTinkerVista.VRChat
 {
     public static class MenuItems
     {
-        const string ExtractDynamicsMenu = "GameObject/Avatar Tinker Vista/Extract VRC PhysBones as ATiV Generate VRM SpringBones";
-        const string ExtractConstraintsMenu = "GameObject/Avatar Tinker Vista/Extract VRC Constraints as ATiV Generate VRM Constraints";
+        const string ExtractDynamicsMenu = "GameObject/Avatar Tinker Vista/Extract VRC PhysBones as ATiV Generate VRM0+1 SpringBones";
+        const string ExtractConstraintsMenu = "GameObject/Avatar Tinker Vista/Extract VRC Constraints as ATiV Generate VRM1 Constraints";
         
         [MenuItem(ExtractDynamicsMenu, true, 60000)]
         [MenuItem(ExtractConstraintsMenu, true, 60001)]
