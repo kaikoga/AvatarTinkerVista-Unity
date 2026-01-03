b869b6266d06c2534975c119bfe0a6ce2ffde6c1
kaikoga <info@mail.kaikoga.net>
2026-01-03 11:54:51 +0900

    add ListComponents tool

---
+++ Editor/Silksprite/AvatarTinkerVista/Tools/ListComponents/ListComponents.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Linq;
+using UnityEngine;
+
+namespace Silksprite.AvatarTinkerVista.Tools.ListComponents
+{
+    [Serializable]
+    public class ListComponents
+    {
+        public Transform avatarRoot;
+        public string[] componentNames = { };
+
+        public void Refresh()
+        {
+            componentNames = avatarRoot.GetComponentsInChildren<Component>()
+                .Where(component => component)
+                .Select(component => component.GetType().FullName)
+                .Distinct()
+                .OrderBy(typeName => typeName)
+                .ToArray();
+        }
+    }
+}
---
+++ Editor/Silksprite/AvatarTinkerVista/Tools/ListComponents/ListComponentsWindow.cs
@@ -0,0 +1,61 @@
+using Silksprite.AvatarTinkerVista.Tools.ReplaceMaterialTexture;
+using UnityEditor;
+using UnityEngine;
+
+namespace Silksprite.AvatarTinkerVista.Tools.ListComponents
+{
+    public class ListComponentsWindow : EditorWindow
+    {
+        Vector2 _scrollPosition = new Vector2(0, 0);
+
+        [SerializeField] ListComponents core = new ListComponents();
+
+        void OnEnable()
+        {
+            titleContent = new GUIContent("List Components");
+
+            var serializedCore = new SerializedObject(this).FindProperty(nameof(core));
+            serializedCore.FindPropertyRelative(nameof(ListComponents.componentNames)).isExpanded = true;
+            serializedCore.serializedObject.ApplyModifiedProperties();
+        }
+
+        void OnGUI()
+        {
+            _scrollPosition = EditorGUILayout.BeginScrollView(_scrollPosition);
+
+            void HelpLabel(string message)
+            {
+                GUILayout.Label(message.Replace(" ", " "), new GUIStyle{wordWrap = true});
+            }
+
+            GUILayout.Label("List Components", new GUIStyle { fontStyle = FontStyle.Bold });
+            GUILayout.Space(4f);
+            EditorGUILayout.HelpBox("使用されているコンポーネントを表示します。".Replace(" ", " "), MessageType.Info);
+            GUILayout.Space(4f);
+
+            HelpLabel("1. アバターを選択する");
+            var serializedCore = new SerializedObject(this).FindProperty(nameof(core));
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(serializedCore.FindPropertyRelative(nameof(ListComponents.avatarRoot)));
+            if (EditorGUI.EndChangeCheck())
+            {
+                serializedCore.serializedObject.ApplyModifiedProperties();
+                core.Refresh();
+            }
+
+            HelpLabel("2. コンポーネント名が表示される");
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUILayout.PropertyField(serializedCore.FindPropertyRelative(nameof(ListComponents.componentNames)));
+            EditorGUI.EndDisabledGroup();
+
+            EditorGUILayout.EndScrollView();
+
+        }
+
+        [MenuItem("Tools/Avatar Tinker Vista/List Components", false, 60000)]
+        static void ShowWindow()
+        {
+            CreateInstance<ListComponentsWindow>().Show();
+        }
+    }
+}
