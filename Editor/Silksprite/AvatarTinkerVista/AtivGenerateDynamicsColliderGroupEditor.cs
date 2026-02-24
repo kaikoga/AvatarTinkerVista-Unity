40553e3812a743e8d8028a66b16279c919644d9f
kaikoga <info@mail.kaikoga.net>
2026-02-25 01:58:52 +0900

    oops

--- Editor/Silksprite/AvatarTinkerVista/AtivGenerateDynamicsColliderGroupEditor.cs
+++ Editor/Silksprite/AvatarTinkerVista/AtivGenerateDynamicsColliderGroupEditor.cs
@@ -11,42 +11,16 @@ namespace Silksprite.AvatarTinkerVista
     [CanEditMultipleObjects]
     class AtivGenerateDynamicsColliderGroupEditor : AtivEditorBase
     {
-        LocalizedProperty _kind;
-        LocalizedProperty _source;
-        LocalizedProperty _target;
-        LocalizedProperty _weight;
-        LocalizedProperty _aimAxis;
-        LocalizedProperty _rollAxis;
+        LocalizedProperty _colliders;
 
         void OnEnable()
         {
-            _kind = Lop(nameof(AtivGenerateConstraint.kind), Loc("AtivGenerateConstraint::kind"));
-            _source = Lop(nameof(AtivGenerateConstraint.source), Loc("AtivGenerateConstraint::source"));
-            _target = Lop(nameof(AtivGenerateConstraint.target), Loc("AtivGenerateConstraint::target"));
-            _weight = Lop(nameof(AtivGenerateConstraint.weight), Loc("AtivGenerateConstraint::weight"));
-            _aimAxis = Lop(nameof(AtivGenerateConstraint.aimAxis), Loc("AtivGenerateConstraint::aimAxis"));
-            _rollAxis = Lop(nameof(AtivGenerateConstraint.rollAxis), Loc("AtivGenerateConstraint::rollAxis"));
+            _colliders = Lop(nameof(AtivGenerateDynamicsColliderGroup.colliders), Loc("AtivGenerateDynamicsColliderGroup.colliders"));
         }
         
         protected override void OnInnerInspectorGUI()
         {
-            LEditorGUILayout.Prop(_kind);
-            LEditorGUILayout.Prop(_source);
-            LEditorGUILayout.Prop(_target);
-            LEditorGUILayout.Prop(_weight);
-            switch ((AtivGenerateConstraint.ConstraintKind)_kind.Property.intValue)
-            {
-                case AtivGenerateConstraint.ConstraintKind.Aim:
-                    LEditorGUILayout.Prop(_aimAxis);
-                    break;
-                case AtivGenerateConstraint.ConstraintKind.Roll:
-                    LEditorGUILayout.Prop(_rollAxis);
-                    break;
-                case AtivGenerateConstraint.ConstraintKind.Rotation:
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
+            LEditorGUILayout.Prop(_colliders);
             serializedObject.ApplyModifiedProperties();
         }
     }
